cb75a8cfbfb5ee426f7c9220081ddf645cad3788
nithinK-142 <nithin.sagar.359@gmail.com>
2024-08-20 10:45:33 +0530

    Initial commit.

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => res.send("Server saying hello!"));
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
